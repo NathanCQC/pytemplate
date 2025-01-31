7c6c422e5e7fe73ad7760338521ec6cb50a38a3b
Kartik Singhal <kartik.singhal@quantinuum.com>
2025-01-31 14:36:59 -0600

    feat: migrate to uv

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,6 @@
 from pytemplate.main import hello_world
 
 
-def test_hello_world():
+def test_hello_world() -> None:
     """Test the hello_world function."""
     assert hello_world() == "Hello, World!"
